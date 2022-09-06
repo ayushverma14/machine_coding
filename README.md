f9134dbbb2b2934a3b3cc88765501e8fb2b62440
ayush verma <verma14ayush@gmail.com>
2022-09-06 13:22:20 +0530

    Test File added,Comments added,Working Fine

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # machine_coding
 Solutions for machine coding round
+1.ParkingLot System
+
+
--- Tests.java
+++ Tests.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+
+// Allowed vehicle types are : Cars,Buses,Bikes
 public class Tests
 {
     public static int free_count(ParkingLot pl)
@@ -44,6 +46,7 @@ public class Tests
         
         return count;
     }
+    // Count of occupied_slots by the given vehicle type
     public static int occupied_slots(ParkingLot pl,String type)
     {
         int count=0;
@@ -62,6 +65,7 @@ public class Tests
         
         return count;
     }
+    // function to park the vehicle
     public static boolean park(ParkingLot pl,String type,String model,String color)
     {
      for(int i=0;i<pl.arr.size();i++)
@@ -83,10 +87,78 @@ return true;
      }   
      return false;
     } 
+
+    /// Method for testing agianst file Input
+    public static void testAgainstFile(String[] file)
+    {
+        ParkingLot pl=null;
+        for(int i=0;i<file.length;i++)
+        {
+            String op[]=file[i].split(" ");
+           if(op.length==4)
+           {
+            if(op[0].equalsIgnoreCase("create_parking_Lot"))
+            {
+
+pl=new ParkingLot(op[1],Integer.parseInt(op[2]),Integer.parseInt(op[3]));
+
+            }
+            else if(op[0].equals("park_vehicle"))
+            {
+                pl.parkingvehicle(op[1],op[2],op[3]);
+            }
+            else if(op[0].equals("unpark_vehicle"))
+            pl.unparkingvehicle(op[1]);
+           }
+           else if(op.length==3)
+           {
+            if(op[0].equals("display"))
+            {
+                if(op[1].equals("free_count"))
+                {
+                    System.out.println("total free count"+":"+free_count(pl));
+                }
+                else if(op[1].equals("free_slot"))
+
+                 {
+                    System.out.println("Free Slot: " + free_slots(pl, op[2]));
+                 }
+                 else if(op[1].equals("occupied_slots"))
+                 System.out.println("Occupied Slot: "+occupied_slots(pl, op[2]));
+                 
+                
+            }
+           }
+           else if(op[0].equals("DisplayLot"))
+           {
+            if(pl==null)
+            System.out.println("Lot empty");
+            else{
+                System.out.println(pl.name);
+                pl.display();
+            }
+           }
+           else if(op[0].equals("exit"))
+           break;
+
+
+        }
+    
+        
+    }
+
+    //Main function to test the code
     public  static void main(String args[]) throws IOException
     {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         ParkingLot pl=null;
+////////// ///////////////////
+// To test the code against the input file mentioned in the inputtxt
+//Paste the array from the TestsInputFile.txt into this file[] array and uncomment thebelow portion
+// String file[]={};      
+// testAgainstFile(file);
+
+///////
         while(true)
         {
             String op[]=br.readLine().split(" ");
@@ -103,7 +175,7 @@ pl=new ParkingLot(op[1],Integer.parseInt(op[2]),Integer.parseInt(op[3]));
                 pl.parkingvehicle(op[1],op[2],op[3]);
             }
             else if(op[0].equals("unpark_vehicle"))
-            pl.unparkingvehicle(op[1],op[2],op[3]);
+            pl.unparkingvehicle(op[1]);
            }
            else if(op.length==3)
            {
@@ -140,6 +212,7 @@ pl=new ParkingLot(op[1],Integer.parseInt(op[2]),Integer.parseInt(op[3]));
         }
     }
 }
+//Slot class
 class Slot
 {
 int occupied;
@@ -154,6 +227,8 @@ this.model=model;
 this.color=color;
 }
 }
+
+// Floor class
 class Floor{
     int slots;
     ArrayList<Slot> arr;
@@ -183,6 +258,7 @@ class Floor{
     }
 
 }
+// Parking Lot class
  class ParkingLot {
     String name;
     int numFloors;
@@ -226,14 +302,14 @@ System.out.println("Lot Created");
         if(f==0)
         System.out.println("Lot full");
     }
-    public void unparkingvehicle(String type,String mod,String color)
+    public void unparkingvehicle(String mod)
     {
         int f=0;
         for(int i=0;i<this.arr.size();i++)
         {
             for(int j=0;j<this.arr.get(i).arr.size();j++)
             {
-                if(this.arr.get(i).arr.get(j).type.equals(type.trim()) && this.arr.get(i).arr.get(j).occupied==1 && this.arr.get(i).arr.get(j).model.equalsIgnoreCase(mod) && this.arr.get(i).arr.get(j).color.equals(color))
+                if(this.arr.get(i).arr.get(j).model.equalsIgnoreCase(mod))
                 {
                     this.arr.get(i).arr.get(j).model="None";
                     this.arr.get(i).arr.get(j).occupied=0;
